c47f51dd91083f7781cfe043f543b0ba7bc50af6
Farukeneszan <146768083+Farukeneszan@users.noreply.github.com>
2025-04-04 19:16:49 +0300

    Add files via upload

---
+++ password.py
@@ -0,0 +1,11 @@
+import random
+karakterler = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+
+uzunluk = int(input("Şifreniz kaç karakterden oluşsun:   "))
+
+sifre = ""
+
+for i in range(uzunluk):
+    sifre = random.choice(karakterler) + sifre
+print(sifre)
+
